7011bb89fe8951992ba00c86c3ecb8ea99a44348
Arash Outadi <arash.out@gmail.com>
2017-02-15 12:04:23 -0800

    From pacelab will put into folder later

---
+++ MECH358/shootingMethod2ndOrder.m
@@ -0,0 +1,7 @@
+function [y1, y2, x] = shootingMethod2ndOrder(f1, f2, b1, b2, xspan)
+%Assuming Dirichet Boundary Conditions
+%Use RK2 built-in during assignment 2
+
+%Step 1: Guess first derivative?
+
+end

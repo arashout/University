e46324bf0aeb3a040ba29b005ac2863a5ceb1554
Arash Outadi <arash.out@gmail.com>
2017-02-20 16:39:04 -0800

    Add files via upload

--- MECH358/runscript.m
+++ MECH358/runscript.m
@@ -1,4 +1,4 @@
-a = @(x) (x + 3)/(x + 1);
+a = @(x) -1*(x + 3)/(x + 1);
 b = @(x) (x + 3)/(x + 1)^2;
 f = @(x) 2*(x + 1) + 3 * b(x);
 %Split 2 second order ODE into 2 first order ODEs
@@ -8,6 +8,11 @@ f2 = @(y1,y2,x) f(x) - b(x)*y1 - a(x)*y2; %=d^2T/dx^2
 TA = [5, 0]; %TA=5 @ x = 0
 TB = [4, 2]; %TB=4 @ x = 2
 %Step size and span
-h = .1;
+h = .01;
 xspan = TA(2):h:TB(2);
-shootingMethod2ndOrder(f1, f2, TA, TB, xspan)
+%Question 1
+[T, Tprime] = shootingMethod2ndOrder(f1, f2, TA, TB, xspan);
+figure(1)
+plot(xspan,T);
+xlabel('x')
+ylabel('T')
--- MECH358/shootingMethod2ndOrder.m
+++ MECH358/shootingMethod2ndOrder.m
@@ -1,6 +1,6 @@
-function [y1, y2, x] = shootingMethod2ndOrder(f1, f2, b1, b2, x)
+function [u, v, x] = shootingMethod2ndOrder(f1, f2, b1, b2, x)
 %Assuming Dirichet Boundary Conditions
-%Use RK2 built-in during assignment 2
+%Use RK4 built-in during assignment 2
 
 %Setup
 tolerance = 10e-3;
@@ -8,24 +8,54 @@ iterations = 50;
 %Get the step size
 h = x(2) - x(1);
 %Pre-allocate row vectors
-y1 = zeros(length(x), 1);
-y2 = zeros(length(x), 1);
+u = zeros(length(x), 1);
+v = zeros(length(x), 1);
 %Set boundary conditions
-y1(1) = b1(1);
-y1(end) = b2(1);
+u(1) = b1(1); %TA(1) == 5
+u(end) = b2(1); %TB(2) == 4
+
+%Start renaming variables in same way as notes LOL
+%Because i am easily confused
+ul = u(end);
 
 %Note: That you need 2 guesses and 2 resulting BC
-y2(1) = rand(1);
-g = y(1); %For previous guess
 
-for i=1:iterations
-    initV = [y1(1), y2(1)]; %Initial Conditions
-    [y1, y2] = rk2Pend(f1, f2, initV , t);
-    if y1(end)^2 < tolerance
+%Guess 1
+v(1) = rand;
+initV = [u(1), v(1)]; %Initial Conditions
+[u, v] = rk4Pend(f1, f2, initV , x);
+v01 = v(1); %first guess
+ul1 = u(end); %T at x=L for first guess
+%Guess 2
+v(1) = rand;
+initV = [u(1), v(1)]; %Initial Conditions
+[u, v] = rk4Pend(f1, f2, initV , x);
+v02 = v(1); %second guess
+ul2 = u(end); %T at x=L for second guess
+
+for i=3:iterations
+    %Make another guess using secant method
+    v03 = v02 + (v02 - v01)/(ul2 - ul1)*(ul-ul2);
+    %SOLVE IVP
+    initV = [u(1), v03]; %Initial Conditions
+    [u, v] = rk4Pend(f1, f2, initV , x);
+    
+    %Check if converged!
+    diff = abs(u(end) - ul);
+    if  diff < tolerance
         string = sprintf('Converged in %d iterations',i);
         disp(string);
-        break
+        return
     end
-    %Make another guess using secant method
+    
+    %Update guess for next secant method
+    ul1 = ul2;
+    ul2 = u(end);
+    v01 = v02;
+    v02 = v03;
 end
+
+string = sprintf('Did not converge');
+disp(string);
+
 end

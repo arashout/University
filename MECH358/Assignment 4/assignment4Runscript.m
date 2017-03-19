57111692217ca0435325c93676de0edd6b940fa7
arashout <arash.out@gmail.com>
2017-03-19 16:11:54 -0700

    Finished with assignment 4

--- MECH358/Assignment 4/assignment4Runscript.m
+++ MECH358/Assignment 4/assignment4Runscript.m
@@ -1,27 +1,54 @@
+path = 'C:\Users\arash\OneDrive\MECHYearThree\MECH358\Homework\';
 %Q1
 %f = @(x) x;
-%f = @(x) x.^2;
-f = @(x) x.^2.*(2.*x-3);
+f = @(x) x.^2;
+%f = @(x) x.^2.*(2.*x-3);
 x0 = 0;
 xL = 1;
 arrN = [1,5,10,100];
 arrAn = zeros(length(arrN), 1);
 h = .01;
 xspan = x0:h:xL;
-path = 'C:\Users\arash\OneDrive\MECHYearThree\MECH358\Homework\';
+figure(1);
+plot(xspan, f(xspan))
+hold on
+plotLines = {'s','*','o','+'};
 for i = 0:length(arrN)-1
     n = arrN(i+1);
     arrAn = findFourierAns(f, n, x0, xL);
     fourier = buildFourierHandle(arrAn, x0, xL);
     yspan = fourier(xspan);
-    figure(1);
-    plot(xspan, yspan)
-    titleString = sprintf('Fourier Approximation for f(x)=x^2*(2*x-3) with N=%d',n);
-    filenameString = strcat(path,sprintf('functionXPoly%d.png',n));
-    title(titleString)
-    xlabel('x')
-    ylabel('y')
-    saveas(1, filenameString, 'png')
+    plot(xspan, yspan, plotLines{i+1})
 end
+hold off
+titleString = 'Fourier Approximation for f(x)=x^2';
+filenameString = strcat(path,'functionX2.png');
+title(titleString)
+xlabel('x')
+ylabel('y')
+legend('Exact Function','N=1','N=5','N=10','N=100','Location','Northwest')
+saveas(1, filenameString, 'png')
 
+%Q3
+fForA1 = @(xs, ts) 1 .* cos(1*pi.*xs) .* exp(-pi^2 * 1^2 .* ts);
+fForA20 = @(xs, ts) -2 .* cos(20*pi.*xs) .* exp(-pi^2 * 20^2 .* ts);
+fTxt = @(xs, ts) fForA1(xs,ts) + fForA20(xs,ts);
+h = .001;
+xsSpan = 0:h:1;
+tsArr = [0, 10^-4, 10^-3, 10^-2, 10^-1];
 
+figure(2)
+hold on
+for i = 1:length(tsArr)
+    tsSpan = ones(1, length(xsSpan)) * tsArr(i);
+    yspan = fTxt(xsSpan, tsSpan);
+    plot(xsSpan, yspan);
+end
+hold off
+title('Fourier series vs time')
+xlabel('x')
+ylabel('y')
+filenameString = strcat(path,'Question3c.png');
+legend('tD_t/L^2=0','tD_t/L^2=10^-4','tD_t/L^2=10^-3','tD_t/L^2=10^-2', ...
+        'tD_t/L^2=10^-1','Location','Southwest')
+saveas(2, filenameString, 'png')

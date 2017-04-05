0ecdec92c52bb8c03b461a95f14ed1b10b3439cd
Arash Outadi <arash.out@gmail.com>
2017-04-05 12:29:43 -0700

    I pretty much have both questions
    But Q1A is way too slow.

--- MECH358/Assignment 5/Assignment5Q1.m
+++ MECH358/Assignment 5/Assignment5Q1.m
@@ -1,22 +1,49 @@
 c1 = .1; %e_dot/(rho*c_p)
 L = 1;
 D_T = 10^-4;
-omega = 10^-3;
-T_o = @(x) 100;
-
+w = 10^-3;
+%T_o is f(x) for bn series
+T_0 = 100;
+f = @(x) T_0;
 tau_D = L^2/D_T;
-N = 10;
-h = .01;
 
-% Find sine series co-efficient
-bnCoeff = findSineCoeff(T_o, N, L);
-% Find Heat generation co-efficients
-snCoeff = zeros(N,1);
-snFunc = @(n, t) @(t) (1 + sin(omega*t)) * -2/(n*pi) * c1 * ( (-1)^n + 1/(n^2*pi^2) * ((-1)^n -1) );
-for n = 1:N
-    snCoeff(n) = snFunc(n);
-end
-
+% Resolution parameters
+N = 1000;
+res = 10;
 
 t = tau_D * [0, 1e-2, 1e-1, 1, 1e1, 1e3];
-x = 0:h:L;
+x = linspace(0,L,res);
+
+%Convenience functions
+yn = @(n) (n*pi/L)^2; %Lambda LOL
+
+% Functions to find sine series co-efficients
+bnIntegrand = @(n) @(x) f(x) .* sin(n * pi * x ./ L);
+bnFunc = @(n) 2 / L * integral(bnIntegrand(n), 0, L);
+sineTerm = @(n,t) bnFunc(n) * exp(-yn(n)*D_T*t);
+%Functions to find heat generation co-efficients
+sn = @(n,t) 4*c1*(1+sin(w*t)) * ((-1)^(n+1) + 1)/(n*pi)^3;
+%Nested function so I can integrate
+snIntegrand = @(n,t) @(tp) sn(n,t) * exp(-yn(n)*D_T*(t - tp));
+heatTerm = @(n,t) integral(snIntegrand(n,t),0,t);
+
+bigBn = @(n,t) sineTerm(n,t) + heatTerm(n,t);
+generalT = @(n,x,t) bigBn(n,t) * sin( sqrt(yn(n)) * x);
+%Make accumalator
+T = zeros(res, 1);
+
+figure(1)
+for j = 1:6
+    %Make accumalator
+    T = zeros(res, 1);
+
+    for i = 1:res
+        for n = 1:N
+            T(i) = T(i) + generalT(n, x(i), t(j));
+        end
+    end
+    plot(x, T);
+    hold on
+end
+
+hold off
--- MECH358/Assignment 5/Assignment5Q2.m
+++ MECH358/Assignment 5/Assignment5Q2.m
@@ -1,20 +1,23 @@
 %RUNSCRIPT Q2
-h = .01; % Mesh grid resolution?
-xS = 0:h:1;
-yS = 0:h:1;
+res = 100;
+xS = linspace(0,1, res);
+yS = linspace(0,1, res);
 [X,Y] = meshgrid(xS,yS);
-% Find Bn terms and build total function
+% Accumalator Matrix
+Z = zeros(res, res);
+% Bn function with T_Hot factored out
 N = 10;
-totalFunction = @(x,y) 0;
-% Notice that THot has been factored out
-BmFunc = @(m) 2/(pi*m)*( (-1)^(m+1) + 1 ) * 1/sinh(m*pi);
-nextFunc = @(xs,ys) sin(n*pi*xs) .* sinh(n*pi*ys);
-for n = 1:N
-    bn = BmFunc(n);
-    totalFunction = @(x,y) totalFunction(x,y) + nextFunc(x,y) * bn;
+bnFunc = @(m) 2/(pi*m)*( (-1)^(m+1) + 1 ) /sinh(m*pi);
+generalFunc = @(xs, ys, n) bnFunc(n) * sin(n*pi*xs) * sinh(n*pi*ys);
+for i = 1:res
+    xs = xS(i);
+    for j = 1:res
+        ys = yS(j);
+        for n = 1:N
+            Z(i,j) = Z(i,j) + generalFunc(xs,ys,n);
+        end
+    end
 end
-Z = totalFunction(X,Y);
-%WTF
-contour(X,Y,Z);
-surf(X,Y, Z);
+%Transpose to fix orientation
+contour(X,Y,Z');
 

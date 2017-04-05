c6b54d86f25779c471e727c5bfc57f90a31da561
arashout <arash.out@gmail.com>
2017-04-04 20:35:38 -0700

    Still Fucked

--- MECH358/Assignment 5/Assignment5Q2.m
+++ MECH358/Assignment 5/Assignment5Q2.m
@@ -1,17 +1,20 @@
 %RUNSCRIPT Q2
-h = .1; % Mesh grid resolution?
+h = .01; % Mesh grid resolution?
 xS = 0:h:1;
 yS = 0:h:1;
 [X,Y] = meshgrid(xS,yS);
-
 % Find Bn terms and build total function
-N = 100;
+N = 10;
 totalFunction = @(x,y) 0;
 % Notice that THot has been factored out
 BmFunc = @(m) 2/(pi*m)*( (-1)^(m+1) + 1 ) * 1/sinh(m*pi);
-nextFunc = @(xs,ys) sin(n*pi*xs) * sinh(n*pi*ys);
+nextFunc = @(xs,ys) sin(n*pi*xs) .* sinh(n*pi*ys);
 for n = 1:N
-    totalFunction = @(x,y) totalFunction(x,y) + nextFunc(x,y) * BmFunc(n);
+    bn = BmFunc(n);
+    totalFunction = @(x,y) totalFunction(x,y) + nextFunc(x,y) * bn;
 end
-contourf(totalFunction(X,Y));
+Z = totalFunction(X,Y);
+%WTF
+contour(X,Y,Z);
+surf(X,Y, Z);
 
